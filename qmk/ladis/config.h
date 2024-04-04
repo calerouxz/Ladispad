58f2d272e7e6b957064609992b33862d203853c5
calerouxz <calerouxz@outlook.com>
2024-04-04 11:10:27 -0400

    updating ladispad code

--- qmk/ladis/config.h
+++ qmk/ladis/config.h
@@ -2,9 +2,6 @@
 #define CONFIG_H
 
 /* USB Device descriptor parameter */
-#define VENDOR_ID       0xFEED
-#define PRODUCT_ID      0x6060
-#define DEVICE_VER      0x0001
 
 /* key matrix size */
 #define MATRIX_ROWS 1
